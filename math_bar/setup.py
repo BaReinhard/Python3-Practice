ae9364aa728f31528c8e1c6d32721ab4deeb9e17
BaReinhard <brett@reinhards.us>
2017-10-30 21:07:05 -0700

    Restructured files for easier use

---
+++ math_bar/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='math_bar',
+      version='0.1',
+      description='Basic math functions',
+      url='http://github.com/bareinhard/Python3-Practice',
+      author='Bareinhard',
+      author_email='brett@reinhards.us',
+      license='MIT',
+      packages=['math_bar'],
+      zip_safe=False)
